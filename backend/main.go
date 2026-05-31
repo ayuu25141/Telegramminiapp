544870e3e8b2604816671d44c3e2a9ba23dc5bb3
Ayush Chauhan <161006131+ayuu25141@users.noreply.github.com>
2026-05-31 20:42:37 +0530

    Update main.go

--- backend/main.go
+++ backend/main.go
@@ -15,59 +15,6 @@ import (
 	"github.com/joho/godotenv"
 )
 
-// func main() {
-// port := os.Getenv("PORT")
-
-// if port == "" {
-//     port = "8080"
-// }
-
-// log.Fatal(app.Listen(":" + port))
-
-// app := fiber.New()
-// app.Use(cors.New(cors.Config{
-//     AllowOrigins: "*",
-//     AllowHeaders: "Origin, Content-Type, Accept, Authorization",
-//     AllowMethods: "GET, POST, PUT, DELETE, OPTIONS",
-//     AllowCredentials: false, // * ke saath false hona chahiye
-// }))
-// // ← Ye add karo CORS ke baad
-// // Preflight ke liye
-// app.Options("/*", func(c *fiber.Ctx) error {
-//     c.Set("Access-Control-Allow-Origin", "*")
-//     c.Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-//     c.Set("Access-Control-Allow-Headers", "*")
-//     return c.SendStatus(204)
-// })
-// fmt.Println("backend start at port 8080")
-
-// // db connection
-
-// if err := dbconnection.Connect()
-// err != nil{
-// 	fmt.Println("error in db connection")
-// }
-// fmt.Println("db connected successfuly")
-
-// bot.Botii()
-// 	app.Get("/", func(c *fiber.Ctx) error {
-// 		return c.SendString("Backend Running")
-// 	})
-
-
-// app.Get("/video/:id",api.Getvideoincollection)	
-// app.Get("/homepagevideos", api.Getvideoinhomepage)
-// app.Get("/profile",protection.Protected)	
-// app.Post("/verifytoken",verifyapi.Checkusertoken)
-
-
-// err :=app.Listen(port)
-// log.Fatal(err)
-
-// }
-
-
-
 
 
 
